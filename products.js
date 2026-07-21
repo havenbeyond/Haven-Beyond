d8926094a21a7e78a5b94d4a6b308324a25e8e1a
havenbeyond <havenbeyond060@gmail.com>
2026-07-21 22:37:01 +0600

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,35 @@
+const products = [
 
+    {
+        image: "product1.jpg",
+        name: "Bubble Electric Foam Blowing Trophy",
+        price: "৳850",
+        description:
+        "Automatic bubble machine with World Cup Trophy design. Perfect for birthdays, parties and kids fun.",
+        whatsapp:
+        "I want to order Bubble Electric Foam Blowing Trophy"
+    },
+
+
+    {
+        image: "product2.jpg",
+        name: "Apple AirPods Max Headphones A2098",
+        price: "৳1300",
+        description:
+        "Premium wireless Bluetooth headphones with stylish design and quality sound.",
+        whatsapp:
+        "I want to order Apple AirPods Max Headphones A2098"
+    },
+
+
+    {
+        image: "product3.jpg",
+        name: "Rabbit Dance Musical Toy",
+        price: "৳1080",
+        description:
+        "Cute dancing rabbit toy with music, LED lights and fun movements.",
+        whatsapp:
+        "I want to order Rabbit Dance Musical Toy"
+    }
+
+];
